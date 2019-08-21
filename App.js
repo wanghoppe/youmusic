859ef40ead43ba49eefb682c4be75dc751863123
hwang <hoppe.wang@move.com>
2019-08-21 13:48:01 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,115 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as Progress from 'react-native-progress';
+import { Audio } from 'expo-av';
+
+function PlayComp(){
+  const soundObject = new Audio.Sound();
+  console.log('Refreshing PlayComp');
+
+
+  return(
+    <View>
+      <Button title="Play Button"
+              onPress={async () => {
+                try{
+                  await Audio.setAudioModeAsync({
+                    staysActiveInBackground: true,
+                    allowsRecordingIOS: false,
+                    interruptionModeIOS : Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS,
+                    playsInSilentModeIOS : false,
+                    interruptionModeAndroid : Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+                    shouldDuckAndroid : true,
+                    playThroughEarpieceAndroid : false,
+                  });
+                  await soundObject.loadAsync({uri: FileSystem.documentDirectory + 'file_example_MP3_5MG.mp3'});
+                  await soundObject.playAsync();
+                } catch (error) {
+                  console.log(error);
+                }
+              }}>
+      </Button>
+    </View>
+  );
+
+}
+
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  const [sample, setSam] = useState('');
+  const [progress, setProg] = useState(0.0);
+  const [visible, setVis] = useState(false);
+  const [finished, setFin] = useState('')
+
+  const downloadResumable = FileSystem.createDownloadResumable(
+    'https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3',
+    FileSystem.documentDirectory + 'file_example_MP3_5MG.mp3',
+    {},
+    (downloadProgress) => {
+      const progress = downloadProgress.totalBytesWritten / downloadProgress.totalBytesExpectedToWrite;
+      setProg(progress);
+    }
+  );
+  // const track = TrackPlayer.setupPlayer()
+  // console.log(TrackPlayer);
+  console.log('dfsafasdfdasdasds dddddddddddddddddddddddddddf                dddddddddddddd')
+  if (visible){
+    test1 = <Progress.Circle
+      progress={progress}
+      size={200}
+      unfilledColor="#fff"
+      color="#ff457f"
+      thickness={2}
+      borderWidth={0} />;
+    <Progress.CircleSnail color={['red', 'green', 'blue']} />;
+  } else {
+    test1 = null;
+  }
+
+  return (
+    <View>
+      <Text>You clicked {count} times</Text>
+      <Button title="Learn More"
+              onPress={() => setCount(count + 1)}>
+      </Button>
+      <Button title="down"
+              onPress={() => setSam(FileSystem.documentDirectory)}>
+      </Button>
+      <Text>{sample}</Text>
+      <Button title="down2"
+              onPress={() => {
+                downloadResumable.downloadAsync()
+                .then(() => {setFin('file_example_MP3_5MG.mp3 Downloaded')});
+                setVis(true);
+              }}>
+      </Button>
+      <Text>{progress}</Text>
+      {test1}
+      <Text>{finished}</Text>
+
+    </View>
+  );
+}
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <Example/>
+      <PlayComp/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
